872921ac3e3a3220799b8ee8178acff1f28ed5d5
RpR-DeeJay <97363461+RpR-DeeJay@users.noreply.github.com>
2026-05-07 02:12:56 +0100

    modified server.py

--- server.py
+++ server.py
@@ -1,9 +1,8 @@
 from flask import Flask, request, render_template
-
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
-	return "hello world"
+	return 
 
 from flask import Flask, request, render_template
 
